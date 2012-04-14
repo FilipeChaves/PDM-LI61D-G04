1c0c8b95243e93e55c05d5bceb8a094f6a258bb3
Filipe <filipe_andrebc@hotmail.com>
2012-04-14 15:41:51 +0100

    Jar Twitter, e melhoramento da apllication

--- androidTestes/src/chaves/android/MyApplication.java
+++ androidTestes/src/chaves/android/MyApplication.java
@@ -2,6 +2,7 @@ package chaves.android;
 
 import winterwell.jtwitter.Twitter;
 import android.app.Application;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.preference.PreferenceManager;
@@ -10,20 +11,30 @@ import android.view.Menu;
 import android.widget.Toast;
 
 public class MyApplication extends Application implements OnSharedPreferenceChangeListener{
-
+	/*Application não deve ter estado, só dados para reduzir o custo entre activities do mesmo processo*/
 	private Twitter _twit;
 	private String TAG;
-	
+
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		TAG = getResources().getString(R.string.hello);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 		prefs.registerOnSharedPreferenceChangeListener(this);
-		openAccount(prefs, false);
+		createAccount(prefs);
 		Log.i(TAG, "MyApplication.onCreate()");
 	}
 	
+	private void createAccount(SharedPreferences prefs) {
+		if(!prefs.contains(getString(R.string.userKey)) || !prefs.contains(getString(R.string.passKey)) || !prefs.contains(getString(R.string.urlKey)))
+			inflatePreferences();
+		openAccount(prefs, false);
+	}
+
+	private void inflatePreferences() {
+		startActivity(new Intent(this, UserPreferencesActivity.class));
+	}
+
 	public Twitter getTwitter(){
 		Log.i(TAG, "MyApplication.getTwitter()");
 		
@@ -42,7 +53,6 @@ public class MyApplication extends Application implements OnSharedPreferenceChan
 		if(!urlChanged)
 			_twit = new Twitter(s.getString(getString(R.string.userKey), ""),
 				s.getString(getString(R.string.passKey), ""));
-		_twit.setAPIRootUrl(s.getString(getString(R.string.urlKey), ""));
+		_twit.setAPIRootUrl(s.getString(getString(R.string.urlKey), getString(R.string.defaultUrl)));
 	}
-
 }
