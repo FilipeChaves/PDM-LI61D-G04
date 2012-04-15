a6b9b2e9f2b2339a5b9e8eef2db2a2490115ba1e
fpita <ffp.pita@gmail.com>
2012-04-15 21:22:43 +0100

    added landscape layout, detail layout

--- fabio/UserStatusActivity.java
+++ fabio/UserStatusActivity.java
@@ -1,8 +1,7 @@
 package chaves.android;
 
 import winterwell.jtwitter.Twitter;
-import android.app.Activity;
-import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,28 +10,51 @@ import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserStatusActivity extends SMActivity implements TextWatcher  {
 	
 	private String BUTTON_TEXT = "buttonText";
 	private String BUTTON_BOOL = "buttonEnable";
 	private String TAG;
+	public int maxChars = 140, aux;
+    public TextView _numCharsText;
+    public Button _button;
+    public EditText _edidText;
+    public InputFilter[] filters = {new InputFilter.LengthFilter(maxChars)};
 	
-	public void init(){
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.main);
+        int orientation = getResources().getConfiguration().orientation;
+        if( orientation == Configuration.ORIENTATION_PORTRAIT){
+        	setContentView(R.layout.main);
+        }
+        else setContentView(R.layout.statuslandscape);
+        init();
+        if(savedInstanceState != null){
+        	_button.setEnabled(savedInstanceState.getBoolean(BUTTON_BOOL));
+        	_button.setText(savedInstanceState.getString(BUTTON_TEXT));
+        }
+        Log.i(TAG, "UserStatusActivity.onCreate()");
+    }
+    public void init(){
 		
 		TAG = getResources().getString(R.string.hello);
 		
 		_edidText = (EditText)findViewById(R.id.textEdit);
         _edidText.setFilters(filters);
         
-        _textButton = (Button)findViewById(R.id.buttonNChars);
-        _textButton.setBackgroundColor(Color.BLACK);
-        _textButton.setTextColor(Color.GREEN);
-        _textButton.refreshDrawableState();
+        _numCharsText = (TextView)findViewById(R.id.buttonNChars);
+        _numCharsText.setBackgroundColor(Color.BLACK);
+        _numCharsText.setTextColor(Color.GREEN);
+        _numCharsText.refreshDrawableState();
         
         _button = (Button) findViewById(R.id.button1);
         _button.setOnClickListener(new View.OnClickListener(){
@@ -62,36 +84,18 @@ public class UserStatusActivity extends SMActivity implements TextWatcher  {
         });
         _edidText.addTextChangedListener(this);
 	}
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        init();
-        if(savedInstanceState != null){
-        	_button.setEnabled(savedInstanceState.getBoolean(BUTTON_BOOL));
-        	_button.setText(savedInstanceState.getString(BUTTON_TEXT));
-        }
-        Log.i(TAG, "UserStatusActivity.onCreate()");
-    }
     
-    public int maxChars = 140, aux;
-    public Button _textButton;
-    public Button _button;
-    public EditText _edidText;
-    public InputFilter[] filters = {new InputFilter.LengthFilter(maxChars)};
     
 	public void afterTextChanged(Editable s) {
 		 aux = maxChars - s.length();
 		 String st = "" + aux;
-         _textButton.setText(st.toCharArray(), 0, st.length());
+		 _numCharsText.setText(st.toCharArray(), 0, st.length());
 
          if(aux > 0)
-        	 _textButton.setTextColor(Color.GREEN);
+        	 _numCharsText.setTextColor(Color.GREEN);
          else
-        	 _textButton.setTextColor(Color.RED);
-         _textButton.refreshDrawableState();
+        	 _numCharsText.setTextColor(Color.RED);
+         _numCharsText.refreshDrawableState();
      }
      public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
      public void onTextChanged(CharSequence s, int start, int before, int count) { }
@@ -117,12 +121,9 @@ public class UserStatusActivity extends SMActivity implements TextWatcher  {
    }
    
    @Override
-   public boolean onOptionsItemSelected(MenuItem item){ //criacao da vista depois de carregado no bota
-	   if(item.getItemId() == R.id.menu_icon_prefs)
-		   startActivity(new Intent(this, UserPreferencesActivity.class));
-	   if(item.getItemId() == R.id.menu_icon_timeline)
-		   startActivity(new Intent(this, TimelineActivity.class));
-	   Log.i(TAG, "SharedMenu.onOptionsItemSelected()");
+	public boolean onCreateOptionsMenu(Menu menu) {  
+	   super.onCreateOptionsMenu(menu);
+	   menu.findItem(R.id.menu_icon_status).setEnabled(false);
 	   return true;
-   }
+	}
 }
