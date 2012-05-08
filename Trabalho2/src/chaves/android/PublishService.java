a2cf9f4a32ba92dc911fd6d1af5ceefbb24c61e3
Filipe <filipe_andrebc@hotmail.com>
2012-05-08 22:18:02 +0100

    Trabalho2

--- Trabalho2/src/chaves/android/PublishService.java
+++ Trabalho2/src/chaves/android/PublishService.java
@@ -6,13 +6,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
+import android.util.Log;
 
 public class PublishService extends Service{
 	
 	public HandlerThread mh;
-	public HandlerService h;
+	public Handler h;
 	public static MyApplication a;
 	
 	@Override
@@ -29,22 +31,28 @@ public class PublishService extends Service{
 		mh = new HandlerThread("PublishServiceThread");
 		
 		mh.start();
+		
+		h = new Handler(mh.getLooper()){
+			@Override
+	    	public void handleMessage(Message msg) {
+	    	   postMessage(((String)msg.obj));
+	    	   stopSelf(msg.arg1);
+	    	}
+		};
 	}
 	
-    class HandlerService extends Handler {
-    	public HandlerService() { super(mh.getLooper()); }
-        @Override
-    	public void handleMessage(Message msg) {
-    	   postMessage(((String)msg.obj));
-    	   stopSelf(msg.arg1);
-    	}	
-    }
-
+//    class HandlerService extends Handler {
+//    	public HandlerService() { super(mh.getLooper()); }
+//        @Override
+//    	public void handleMessage(Message msg) {
+//    	   postMessage(((String)msg.obj));
+//    	   stopSelf(msg.arg1);
+//    	}	
+//    }
 
 	public void postMessage(Object msg) {
 		a.getTwitter().updateStatus(msg.toString());
 	}
-	
 	
 	@Override
 	public void onDestroy() {
@@ -52,14 +60,14 @@ public class PublishService extends Service{
 		mh.stop();
 	}
 
-
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
 		
 		final String s = intent.getStringExtra("TwitterMessage");
-		Message msg = new Message();
+		Message msg = Message.obtain();
 		msg.obj = s;
+		
 		h.sendMessage( msg );
 		return 1;
 	}
